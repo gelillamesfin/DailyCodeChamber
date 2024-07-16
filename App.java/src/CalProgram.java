c3b1d763896ba3a2d914d25d7c4dd14d61d7ba0a
Gelilla Tafa <gelilamesfin13@yahoo.com>
2024-07-16 18:07:54 -0500

    multipler

---
+++ App.java/src/CalProgram.java
@@ -0,0 +1,7 @@
+public class CalProgram {
+public static void main(String[] args) {
+    Multiplier mult= new Multiplier();
+    int result=mult.multiply(4, 12130, 0435);
+    System.out.println("Result of Multiplier: " + result);
+}
+}
