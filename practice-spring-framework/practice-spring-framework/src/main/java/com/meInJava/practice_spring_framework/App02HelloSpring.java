2be9ed572180d4129c3732710cad3b190ee2178c
Gelilla Tafa <gelilamesfin13@yahoo.com>
2024-08-09 15:22:01 -0500

    spring

---
+++ practice-spring-framework/practice-spring-framework/src/main/java/com/meInJava/practice_spring_framework/App02HelloSpring.java
@@ -0,0 +1,22 @@
+package com.meInJava.practice_spring_framework;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloSpring {
+    public static void main(String[] args) {
+      
+var context= new AnnotationConfigApplicationContext(HelloSpringConfig.class);
+// context.getBean("name");
+// System.out.println(context.getBean("name"));
+// System.out.println((context.getBean("age")));
+// System.out.println(context.getBean("person2"));
+// System.out.println(context.getBean("person"));
+// System.out.println(context.getBean(Address.class));
+// System.out.println(context.getBeanDefinitionCount());
+Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+// System.out.println(context.getBeanDefinitionNames());
+}
+
+}
---
+++ practice-spring-framework/practice-spring-framework/src/main/java/com/meInJava/practice_spring_framework/HelloSpringConfig.java
@@ -0,0 +1,39 @@
+package com.meInJava.practice_spring_framework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+record Person(String name, int age , Address address){
+}
+record Address (String street ,String city, int zipcode){
+
+}
+@Configuration
+public class HelloSpringConfig {
+    @Bean
+public String name (){
+    return "Gelilla";
+}
+@Bean
+public int age(){
+    return 100;
+}
+@Bean 
+public Person person(){
+    var person= new Person("Lulu",300, new Address("main st ","FairField",4123));
+    
+    return person;
+}
+
+@Bean
+public Person person2() {
+    var person = new Person(name(), age(),address());
+
+    return person;
+}
+@Bean(name="address2")
+public Address address(){
+return  new Address("Here st ", "Dallas", 75081);
+ 
+}
+
+}
